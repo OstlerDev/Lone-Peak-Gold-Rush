2b86615a9857df978093db9e3075442ab483584e
Josh Ostler <ostlerdev@me.com>
2013-10-29 21:46:07 -0600

    Initial Commit
    Base design and some admin functions implemented.

---
+++ goldrush.js
@@ -0,0 +1,15 @@
+
+
+if (Meteor.isClient) {
+Meteor.subscribe("goldRush");
+  Template.home.total = function () {
+    return 'null';
+    //return GoldRush.find({total: 1});
+  }
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
